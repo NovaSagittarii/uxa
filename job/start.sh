762f6b40137f644665d4df3ed0410cd4f2b6501c
Thomas Li <tli186@ucr.edu>
2026-04-19 00:32:51 -0600

    more graceful ratelimit handling
    works well with gemini gemma 4 15 rpm with no token limits

--- job/start.sh
+++ job/start.sh
@@ -11,6 +11,11 @@ cp ~/.zeroclaw/config.toml.tmp ~/.zeroclaw/config.toml
 toml set ~/.zeroclaw/config.toml agent.max_tool_iterations 100 > ~/.zeroclaw/config.toml.tmp
 cp ~/.zeroclaw/config.toml.tmp ~/.zeroclaw/config.toml
 
+toml set ~/.zeroclaw/config.toml reliability.max_retries 100 > ~/.zeroclaw/config.toml.tmp
+cp ~/.zeroclaw/config.toml.tmp ~/.zeroclaw/config.toml
+toml set ~/.zeroclaw/config.toml reliability.max_backoff_ms 60000 > ~/.zeroclaw/config.toml.tmp
+cp ~/.zeroclaw/config.toml.tmp ~/.zeroclaw/config.toml
+
 # exec bash
 exec zeroclaw agent
 
