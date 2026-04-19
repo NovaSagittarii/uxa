785a3329933e6b87d7288b06a6cb6ab227990649
Thomas Li <tli186@ucr.edu>
2026-04-19 02:38:31 -0600

    i guess don't have the server run early...

--- job/start.sh
+++ job/start.sh
@@ -17,7 +17,9 @@ toml set ~/.zeroclaw/config.toml reliability.max_backoff_ms 60000 > ~/.zeroclaw/
 cp ~/.zeroclaw/config.toml.tmp ~/.zeroclaw/config.toml
 
 # exec bash
-/root/.local/bin/uv run /uxa-job/main.py &
-zeroclaw agent -m $ZEROCLAW_QUERY > /uxa-job/out.txt 2> /uxa-job/err.txt
+# /root/.local/bin/uv run /uxa-job/main.py &
+# zeroclaw agent -m $ZEROCLAW_QUERY > /uxa-job/out.txt 2> /uxa-job/err.txt
+# exec wait
 
-exec wait
+zeroclaw agent -m $ZEROCLAW_QUERY > /uxa-job/out.txt 2> /uxa-job/err.txt
+exec /root/.local/bin/uv run /uxa-job/main.py
